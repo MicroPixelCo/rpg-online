1c7b4544344ea5eb6897dad983ed13d2ef973157
MicroPixelCo <lorenzo.vf2704@gmail.com>
2026-04-14 11:45:41 -0300

    disable CSP to allow inline scripts and event handlers

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ require("dotenv").config();
 const app = express();
 
 // ===== MIDDLEWARE =====
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
